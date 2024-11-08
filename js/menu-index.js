dc5565ee72190265ce9bc4bab84f1fcc0f2f6553
ssh-lvl <roseslucerob@nclack.org>
2024-11-08 11:46:17 -0800

    Update menu-index.js

--- js/menu-index.js
+++ js/menu-index.js
@@ -8,7 +8,8 @@ const menuItems = [
     {name: 'Docs', action: '../Docs/Index.html'},
     {name: 'Frat-Chat', link: '../chat.html'},
     {name: 'Screenshot', action: 'screenshot', class: 'screenshot'},
-    {name: 'Reload', action: 'reloadPage', class: 'reload'}
+    {name: 'Reload', action: 'reloadPage', class: 'reload'},
+    {name: 'Cloak', action: 'Cloak', class: 'cloak'}
 ];
 
 const menuCenterX = 100; // X coordinate of the menu center
@@ -92,6 +93,29 @@ function handleMenuItemClick(item) {
                 break;
             case 'reloadPage':
                 location.reload();
+                break;
+            case 'Cloak':
+                var url = document.getElementById("url-target");
+                var urlObj = new window.URL(window.location.href);
+                document.getElementById("create").onclick = function () {
+                if (url.value.substring(0, 8) !== 'https://' && url.value.substring(0, 7) !== 'http://') {
+            		url.value = 'https://' + url.value.split('https://').pop();
+                  } else if (url.value.substring(0, 7) == 'http://'){
+        	        url.value = 'https://' + url.value.split('http://').pop();
+                 };
+                win = window.open('about:blank', 'about:blank', 'height=600,width=800,resizable=no,scrollbars=no,status=no,toolbar=no,location=no,menubar=no');
+                win.document.body.style.margin = "0";
+                win.document.body.style.height = "100vh";
+                var iframe = win.document.createElement("iframe");
+                iframe.style.border = "none";
+                iframe.style.width = "100%";
+                iframe.style.height = "100%";
+                iframe.style.margin = "0";
+                iframe.referrerpolicy = "no-referrer";
+                iframe.allow = "fullscreen";
+                iframe.src = url.value;
+                win.document.body.appendChild(iframe);
+                }
                 break;
         }
     }
