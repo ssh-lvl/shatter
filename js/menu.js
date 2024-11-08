e7971eafc20a50bf36935cdc555ef253b2232c40
ssh-lvl <roseslucerob@nclack.org>
2024-11-08 12:12:06 -0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+//import 'cloak.js'
 let menuOpen = false;
 let informationShown = false;
 const menuItems = [
@@ -95,27 +96,7 @@ function handleMenuItemClick(item) {
                 location.reload();
                 break;
             case 'cloak':
-                var url = document.getElementById("url-target");
-      var urlObj = new window.URL(window.location.href);
-      document.getElementById("create").onclick = function () {
-        if (url.value.substring(0, 8) !== 'https://' && url.value.substring(0, 7) !== 'http://') {
-    		url.value = 'https://' + url.value.split('https://').pop();
-        } else if (url.value.substring(0, 7) == 'http://'){
-        	url.value = 'https://' + url.value.split('http://').pop();
-        };
-        win = window.open();
-        win.document.body.style.margin = "0";
-        win.document.body.style.height = "100vh";
-        var iframe = win.document.createElement("iframe");
-        iframe.style.border = "none";
-        iframe.style.width = "100%";
-        iframe.style.height = "100%";
-        iframe.style.margin = "0";
-        iframe.referrerpolicy = "no-referrer";
-        iframe.allow = "fullscreen";
-        iframe.src = url.value;
-        win.document.body.appendChild(iframe);
-      }
+                
                 break;
         }
     }
@@ -176,6 +157,29 @@ function setInitialMenuIcon() {
     menuIcon.style.height = '50px';
     menuIcon.style.backgroundSize = 'cover';
     menuIcon.style.transition = 'transform 0.3s ease-out';
+}
+function cloakpage() {
+    var url = document.getElementById("url-target");
+      var urlObj = new window.URL(window.location.href);
+      document.getElementById("create").onclick = function () {
+        if (url.value.substring(0, 8) !== 'https://' && url.value.substring(0, 7) !== 'http://') {
+    		url.value = 'https://' + url.value.split('https://').pop();
+        } else if (url.value.substring(0, 7) == 'http://'){
+        	url.value = 'https://' + url.value.split('http://').pop();
+        };
+        win = window.open();
+        win.document.body.style.margin = "0";
+        win.document.body.style.height = "100vh";
+        var iframe = win.document.createElement("iframe");
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.margin = "0";
+        iframe.referrerpolicy = "no-referrer";
+        iframe.allow = "fullscreen";
+        iframe.src = url.value;
+        win.document.body.appendChild(iframe);
+      };
 }
 
 // Call this function after the DOM is fully loaded
